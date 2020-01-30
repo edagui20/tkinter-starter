4cb68fd64ee31ca9dc987e5dac56c434379f847b
Efren Aguilar-Hernandez <edagui20>
2020-01-30 11:16:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-# A starter program for Python with Tkinter
 
-from tkinter import * # import Tkinter library
-window = Tk()         # Create the application window
 
-# Add a label with the text "Hello"
-lbl = Label(window, text="Bucket")
+from tkinter import * 
+from tkinter.ttk import Progressbar
+from tkinter import ttk
+from tkinter import Menu
+window = Tk()         
 
-# Place the label in the window at 0, 0
+
+window.title("Blue")
+lbl = Label(window, text="Fun", font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
-
-
-window.mainloop()     # Keep the window open
+lbl = Label(window, text="Game", font=("Arial Bold", 20))
+lbl.grid(column=0, row=2)
+txt= Entry(window,width=20)
+txt.grid(column=5, row=0)
+txt= Entry(window,width=20)
+txt.grid(column=5, row=2)
+chk_state = BooleanVar()
+chk_state.set(True)
+chk = Checkbutton(window, text='Cool', var=chk_state)
+chk.grid(column=0, row=4)
+chk_state = BooleanVar()
+chk_state.set(True)
+chk = Checkbutton(window, text='Not Cool', var=chk_state)
+chk.grid(column=0, row=5)
+style = ttk.Style()
+style.theme_use('default')
+style.configure("black.Horizontal.TProgressbar", background='black')
+bar = Progressbar(window, length=200, style='black.Horizontal.TProgressbar')
+bar['value']=70
+bar.grid(column=3, row=7)
+menu = Menu(window)
+new_item = Menu(menu)
+new_item.add_command(label='new')
+new_item.add_separator()
+new_item.add_command(label='Edit')
+menu.add_cascade(label='File', menu=new_item)
+window.config(menu=menu)
+def clicked():
+    lbl.configure(text="A virus has been intalled")
+btn = Button(window, text="Click to install", command=clicked)
+btn.grid(column=1, row=0)
+def clicked():
+    lbl.configure(text="Installing Virus")
+btn = Button(window, text="Click to get a virus", command=clicked)
+btn.grid(column=1, row=2)
+window.mainloop()     
